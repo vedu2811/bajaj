3c856ae29a68fb77c3ffaa932519969195b77a69
Vedant <vedant28j@gmail.com>
2025-08-29 06:01:02 +0000

    importing cors and adding middlewares

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is Working");
